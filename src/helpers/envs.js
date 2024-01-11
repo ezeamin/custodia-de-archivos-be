ea72e158b99666a81c8e71b36684a7ebbd848dfb
Ezequiel Amin <ezequielamin@outlook.com>
2024-01-11 19:03:05 -0300

    fix(envs): improve configs of envs files

--- src/helpers/envs.js
+++ src/helpers/envs.js
@@ -7,7 +7,7 @@ export const envs = {
   DATABASE_URL: get('DATABASE_URL').required().asString(),
   DIRECT_URL: get('DIRECT_URL').required().asString(),
   JWT_SECRET_KEY: get('JWT_SECRET_KEY').required().asString(),
-  CLIENT_URL: get('CLIENT_URL').required().asString(),
+  CLIENT_URL: get('CLIENT_URL').required().asUrlString(),
   MAIL: {
     USER: get('MAIL_USER').required().asString(),
     PASS: get('MAIL_PASS').required().asString(),
