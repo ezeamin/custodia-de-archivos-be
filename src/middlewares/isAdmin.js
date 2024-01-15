879d01b614e4c8a073529207759bd3db5d2d6b45
Ezequiel Amin <ezequielamin@outlook.com>
2024-01-15 15:44:17 -0300

    fix(global): remove unused code and update deps

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,9 +1,10 @@
 import HttpStatus from 'http-status-codes';
+import { roles } from '../constants/roles';
 
 export const isAdmin = (req, res, next) => {
   const { user } = req;
 
-  if (!user.isAdmin) {
+  if (user.role !== roles.ADMIN) {
     res.status(HttpStatus.FORBIDDEN).json({
       data: null,
       message: 'No tienes permisos para realizar esta acción',
