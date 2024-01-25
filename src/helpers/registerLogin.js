a678b73bf0938e973eb421e422216b73934125d4
Ezequiel Amin <ezequielamin@outlook.com>
2024-01-25 09:48:53 -0300

    fix(auth): improve ip logging

--- src/helpers/registerLogin.js
+++ src/helpers/registerLogin.js
@@ -3,13 +3,13 @@ import { prisma } from './prisma.js';
 export const registerLogin = async (req, id_user) => {
   const {
     ip,
-    headers: { 'user-agent': userAgent },
+    headers: { 'user-agent': userAgent, 'true-client-ip': trueClientIp },
   } = req;
 
   try {
     await prisma.login.create({
       data: {
-        ip_address: ip,
+        ip_address: ip === '::1' ? trueClientIp : 'Desconocido',
         user_agent: userAgent,
         id_user,
       },
